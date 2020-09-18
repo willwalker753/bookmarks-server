f69f311a1692ea597b70b3f7ba9c96f1957cb649
Will Walker <williamwalker753@gmail.com>
2020-09-18 13:05:22 -0500

    typo fixed

--- src/validateBearerToken.js
+++ src/validateBearerToken.js
@@ -1,12 +1,10 @@
 function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
-    console.log(apiToken);
     const authToken = req.get('Authorization')
-    console.log(authToken);
     if (!authToken || authToken !== apiToken) {
-        //return res.status(401).json({ error: 'Unauthorized request' })
+        return res.status(401).json({ error: 'Unauthorized request' })
     }
     next()
 }
 
-module.exports = validateBearerToken
+module.exports = validateBearerToken;
